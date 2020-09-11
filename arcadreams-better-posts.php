2b38c129bbfa8407c0f9278c736e1853b4d6278e
arcadreams <71049931+arcadreams@users.noreply.github.com>
2020-09-11 21:13:54 +0200

    Create arcadreams-better-posts.php

---
+++ arcadreams-better-posts.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Arca Dreams Better Posts
+Plugin URI: https://arcadreams.com
+Description: Shortcodes sencillos para crear diseños mejorados en tus Posts
+Version: 1.0.0
+Author: Arca Dreams
+Author URI: https://arcadreams.com
+License: LicenciaGPL
+Text domain: arcadreams-better-posts
+*/
