98ea2a540fc09e95106ad6cb83af666bb523b678
arcadreams <71049931+arcadreams@users.noreply.github.com>
2020-09-12 00:20:33 +0200

    Update arcadreams-better-posts.php

--- arcadreams-better-posts.php
+++ arcadreams-better-posts.php
@@ -3,7 +3,7 @@
 Plugin Name: Arca Dreams Better Posts
 Plugin URI: https://arcadreams.com
 Description: Publicaciones mejoradas mediante Shortcodes sencillos para usar en tus Posts.
-Version: 1.0.0
+Version: 1.0.1
 Author: Arca Dreams
 Author URI: https://arcadreams.com
 License: LicenciaGPL
